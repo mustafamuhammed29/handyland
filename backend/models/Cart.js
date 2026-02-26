354493dac91097f974b8ccc54f2a5d83125bb347
Mustafa Muhammed <83115972+mustafamuhammed29@users.noreply.github.com>
2026-02-26 23:03:22 +0100

    fix(backend): remove buggy pre-save hook on Cart model to allow items to be saved

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -37,10 +37,4 @@ const cartSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-// Middleware to update updatedAt
-cartSchema.pre('save', function (next) {
-    this.updatedAt = Date.now();
-    next();
-});
-
 module.exports = mongoose.model('Cart', cartSchema);
