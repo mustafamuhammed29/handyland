87cebaf9bac48a9c21e246699de77dfd21804e27
Mustafa Muhammed <83115972+mustafamuhammed29@users.noreply.github.com>
2026-03-18 23:14:42 +0100

    s

---
+++ backend/fix_images.js
@@ -0,0 +1,45 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+async function fix() {
+  try {
+    const client = await MongoClient.connect(process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/handyland');
+    const db = client.db();
+    
+    await db.collection('products').updateMany(
+      { image: { $regex: '^http://' } },
+      [{
+        $set: {
+          image: {
+            $replaceOne: {
+              input: "$image",
+              find: "http://127.0.0.1:5000",
+              replacement: ""
+            }
+          }
+        }
+      }]
+    );
+    
+    await db.collection('repairdevices').updateMany(
+      { image: { $regex: '^http://' } },
+      [{
+        $set: {
+          image: {
+            $replaceOne: {
+              input: "$image",
+              find: "http://127.0.0.1:5000",
+              replacement: ""
+            }
+          }
+        }
+      }]
+    );
+    console.log("Fixed image paths successfully");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+fix();
